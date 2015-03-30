a844cd6dface93d802355f2f2446774763a7dc57
Norm Paradis <nparadis@sasktech.ca>
2015-03-29 23:12:51 -0600

    Started Console Sample

---
+++ RPGL-Console-Sample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RandomPasswordGenerator;
+
+namespace RPGL_Console_Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+        }
+    }
+}
